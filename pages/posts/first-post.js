c238ac0dbc60fd5c745c28b5ad0ae73cbd11d5f9
Farid Vatani <faridvatani@gmail.com>
2022-08-03 06:58:41 +0430

    Assets, Metadata, and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,30 @@
+import Head from 'next/head';
 import Link from 'next/link';
-import styles from '../../styles/Home.module.css'
+import Layout from '../../components/layout';
+import styles from '../../styles/Home.module.css';
+import Script from 'next/script';
+
 
 export default function FirstPost() {
   return (
-    <>
+      <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+        <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+        />
+
       <h1 className={styles.title}>First Post</h1>
       <h2 className={styles.title}>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+      </Layout>
   );
 }
